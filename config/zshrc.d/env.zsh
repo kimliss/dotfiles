1b7d44c443e0b31255306270f1e5323844f35684
Kim Liss <itwangxiang@gmail.com>
2025-11-09 16:38:18 +0800

    Create env.zsh for environment variable setup
    Add environment variable configuration for zsh.

---
+++ config/zshrc.d/env.zsh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+# env.zsh - 环境变量配置
+
+# 编辑器
+export EDITOR=vim
+export VISUAL=vim
+
+# 历史记录
+export HISTSIZE=10000
+export SAVEHIST=10000
+export HISTFILE=~/.zsh_history
+
+# 语言设置
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+# 路径
+# export PATH="$HOME/bin:$PATH"

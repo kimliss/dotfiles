53828ac82fafa41040091fda016496ec57c2b973
kim <idkimliss@icloud.com>
2026-09-16 10:16:52 +0800

    cancel download zsh-syntax-highlighting, zsh-autosuggestions

--- run_once_install-oh-my-zsh.sh
+++ run_once_install-oh-my-zsh.sh
@@ -13,17 +13,3 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
 
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
-
-# zsh-autosuggestions
-if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
-    git clone \
-        https://github.com/zsh-users/zsh-autosuggestions \
-        "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
-fi
-
-# zsh-syntax-highlighting
-if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
-    git clone \
-        https://github.com/zsh-users/zsh-syntax-highlighting.git \
-        "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
-fi
